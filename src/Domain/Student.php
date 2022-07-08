b664d18318030af6756206fc7c4b1703358fb943
tiagoliveirati <dev.tiago.oliveira@gmail.com>
2022-07-08 12:23:34 -0300

    feat: ensure Student have a name

--- src/Domain/Student.php
+++ src/Domain/Student.php
@@ -1,7 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tiagoliveirati\CleanArch\Domain;
 
 class Student
 {
+    public function __construct(private string $name)
+    {
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
